030786db0f7c2ab5a4752199f1e8616fd633f1f2
diefrompain1993 <aleksei.bat18@mail.ru>
2025-06-24 04:41:48 +0300

    Fix constructor optional chaining and fetch orders

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,27 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchFeeds } from '../../services/orders/slice';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const { feeds, loading } = useSelector((state) => state.orders);
 
-  if (!orders.length) {
+  useEffect(() => {
+    if (!feeds) {
+      dispatch(fetchFeeds());
+    }
+  }, [dispatch, feeds]);
+
+  if (loading || !feeds) {
     return <Preloader />;
   }
 
-  return <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return (
+    <FeedUI
+      orders={feeds.orders}
+      handleGetFeeds={() => dispatch(fetchFeeds())}
+    />
+  );
 };
